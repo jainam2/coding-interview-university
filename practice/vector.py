911ca5dcf42ce8d5ced42bb52a98c71129d6a704
Your Name <you@example.com>
2020-08-28 21:35:03 +0530

    Adding practice problem done

---
+++ practice/vector.py
@@ -0,0 +1,81 @@
+class Vector:
+	
+	def __init__(self, size = 16):
+		self._vector = [0 for i in range(size)]
+		self._capacity = size
+		self._size = 0
+
+	def _resize(self, capacity):
+		new_vec = [0 for i in range(capacity)]
+		self._capacity = capacity
+		for i in range(self._size):
+			new_vec[i] = self._vector[i]
+		self._vector = new_vec
+
+	def size(self):
+		return self._size
+
+	def capacity(self):
+		return self._capacity
+
+	def is_empty(self):
+		return self._size == 0
+
+	def at_index(self, index):
+		if index >= self._size:
+			return "Index range out of bounds"
+
+		return self._vector[index]
+
+	def push(self, item):
+		if (self._size == self._capacity):
+			self._resize(self._size * 2)
+
+		self._vector[self._size] = item
+		self._size += 1
+	
+	def insert(self, index, item):
+		if (self._size == self._capacity):
+			self._resize(self._capacity * 2)
+		
+		prev = item
+		for i in range(index, self._size+1):
+			temp = self._vector[i]
+			self._vector[i] = prev
+			prev = temp
+		self._size += 1
+
+	def pop(self):
+		if (self._size == 0):
+			return "No elements present"
+		self._vector[self._size - 1] = 0
+		self._size -= 1
+
+	def delete(self, index):
+		if index >= self._size:
+			return "Index out of bounds"
+
+		for i in range(index, self._size-1):
+			self._vector[i], self._vector[i+1] = self._vector[i+1], self._vector[i]
+		self.pop()
+
+	def find(self, item):
+		if self._size == 0:
+			return 0
+
+		for i in range(self._size):
+			if self._vector[i] == item:
+				return i
+		return 0
+
+	def remove(self, item):
+		res = self.find(item)
+		if res:
+			self.delete(res)
+		return res
+
+if __name__ == "__main__":
+	import doctest
+	print("Testing the module....")
+	doctest.testmod()
+	print("Tested the module")
